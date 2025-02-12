0a72f6888e71559d96deef43f6c513eb76a79d0b
vivekpatel99 <vivpatel99@gmail.com>
2025-02-12 07:53:15 +0000

    prepare dataset upated

--- utils/prepare_dataset.py
+++ utils/prepare_dataset.py
@@ -2,12 +2,13 @@ import os
 from pathlib import Path
 
 import numpy as np
+import tensorflow as tf
 import tqdm
 from tqdm.notebook import tqdm
 
 
 class PrepareDataset:
-    def __init__(self, image_dir: Path, label_dir: Path) -> None:
+    def __init__(self, image_dir: Path, label_dir: Path, dst_img_size:tuple[int, int]) -> None:
         """
         Args:
             image_dir (str): Path to the directory containing the images.
@@ -15,7 +16,15 @@ class PrepareDataset:
         """
         self.image_dir = image_dir
         self.label_dir = label_dir
+        self.dst_img_size= dst_img_size
 
+    def prepare_images(self, image_path:str):
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_jpeg(image, channels=3)
+        resize_img = tf.image.resize(image, self.dst_img_size)
+        scaled_img = resize_img / 255.
+        return tf.cast(scaled_img, tf.float32)
+    
     def get_dataset(self) -> tuple[list[str], list[int], list[np.ndarray]]:
         """Loads and parses YOLOv8 labels.
 
@@ -33,7 +42,8 @@ class PrepareDataset:
         bboxes = []
         for file_name in tqdm(self.image_dir.iterdir()):
             if file_name.suffix.endswith((".jpg", ".png")):
-                imaga_file_path = file_name
+                imaga_file_path = str(file_name)
+                # preprocessed_img = self.prepare_images(image_path=imaga_file_path)
                 label_file_path = self.label_dir / f'{file_name.stem}.txt'
 
                 if not label_file_path.exists():
@@ -55,19 +65,28 @@ class PrepareDataset:
                     try:
                         values = np.array([value for value in line.split()], dtype=np.float32)
                         class_id = int(values[0])
-                        coords = values[1:]  # Coords are already normalized YOLO format
+                        coords = values[1:9]  # Coords are already normalized YOLO format
 
-                        # Reshape coords to (1, 4) if it's a single box
-                        if coords.size == 4:
-                            coords = coords.reshape(1, 4)  # Ensure it's a 2D array even for one box
+                        if coords.size == 8:
+                            coords = coords.reshape(4, 2)  # Ensure it's a 2D array with four points
+
+                        # Calculate xmin, ymin, xmax, ymax
+                        x_coords = coords[:, 0]
+                        y_coords = coords[:, 1]
+                        xmin = np.min(x_coords)
+                        ymin = np.min(y_coords)
+                        xmax = np.max(x_coords)
+                        ymax = np.max(y_coords)
+
 
                         # image_bboxes.append(coords)
                         # image_classes.append(class_id)
                         # image_paths.append(str(imaga_file_path))
 
-                        bboxes.append(coords)
+                        bboxes.append(np.array([[xmin, ymin, xmax, ymax]]))
                         class_ids.append(class_id)
-                        image_paths.append(str(imaga_file_path))
+                        image_paths.append(imaga_file_path)
+
                     except Exception as e:
                         print(f"[ERROR] - {e} in file {label_file_path} on line: {line}")
                         continue
