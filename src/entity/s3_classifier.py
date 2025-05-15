b055da7718ea41bcd5870b9486c0c282d4b470f5
vivekpatel99 <vivpatel99@gmail.com>
2025-05-15 13:10:12 +0200

    evaluation and model fetching and pushing done

--- src/entity/s3_classifier.py
+++ src/entity/s3_classifier.py
@@ -22,18 +22,20 @@ class S3Classifier:
     def __init__(
         self,
         bucket_name,
-        model_path,
+        cloud_model_key,
+        cloud_model_save_path,
     ):
         """
         :param bucket_name: Name of your model bucket
         :param model_path: Location of your model in bucket
         """
         self.bucket_name = bucket_name
-        self.model_path = model_path
+        self.cloud_model_key = cloud_model_key
+        self.cloud_model_save_path = cloud_model_save_path
         self.s3 = S3Bucket()
         self.loaded_model: pl.LightningModule = None
 
-    def is_model_present(self, model_path: str) -> bool:
+    def is_model_present(self, model_name: str) -> bool:
         """
         Checks if a specified S3 key path (file path) is available in the specified bucket.
 
@@ -44,12 +46,12 @@ class S3Classifier:
             bool: True if the file exists, False otherwise.
         """
         try:
-            return self.s3.s3_key_path_available(bucket_name=self.bucket_name, s3_key=model_path)
+            return self.s3.s3_key_path_available(key=model_name)
         except Exception as e:
-            print(e)
+            log.error(e)
             return False
 
-    def _load_model(self) -> pl.LightningModule:
+    def fetch_model_weights(self) -> str | bool:
         """
         Loads a serialized model from the specified S3 bucket.
 
@@ -57,12 +59,12 @@ class S3Classifier:
             object: The deserialized model object.
         """
         try:
-            model_weigthts = self.s3.load_model_weights(model_name=self.model_path, bucket_name=self.bucket_name)
-            print(model_weigthts)
+            return self.s3.fetch_model_weights(cloud_model_key=self.cloud_model_key, model_save_path=self.cloud_model_save_path)
         except Exception as e:
-            print(e)
+            log.error(e)
+            return False
 
-    def save_model(self, from_file, remove: bool = False) -> None:
+    def upload_model(self, from_file, remove: bool = False) -> None:
         """
         Save the model to the model_path
         :param from_file: Your local system model path
@@ -71,27 +73,18 @@ class S3Classifier:
         """
         try:
             log.info("Saving model to S3 bucket")
-            self.s3.upload_file(from_file, to_filename=self.model_path, bucket_name=self.bucket_name, remove=remove)
+            self.s3.upload_file(from_file, to_filename=self.cloud_model_key, bucket_name=self.bucket_name, remove=remove)
         except Exception as e:
-            print(e)
+            log.error(e)
         log.info("Model saved to S3 bucket")
-
-    # def predict(self, dataframe: DataFrame):
-    #     """
-    #     :param dataframe:
-    #     :return:
-    #     """
-    #     try:
-    #         if self.loaded_model is None:
-    #             self.loaded_model = self._load_model()
-    #         return self.loaded_model.predict(dataframe=dataframe)
-    #     except Exception as e:
-    #         print(e)
 
 
 if __name__ == "__main__":
     secrets = AwsSecrets()
     bucket_name = secrets.bucket_name
-    s3_classifier = S3Classifier(bucket_name=bucket_name, model_path="epoch_001.ckpt")
+    s3_classifier = S3Classifier(bucket_name=bucket_name, cloud_cloud_model_save_path="results")
     file_path = "logs/train/runs/2025-05-14_12-56-57/checkpoints/epoch_001.ckpt"
-    s3_classifier.save_model(from_file=file_path)
+    # s3_classifier.save_model(from_file=file_path)
+    result = s3_classifier.is_model_present(model_path=file_path)
+    print(result)
+    model_weights = s3_classifier.fetch_model_weights()
--- src/eval.py
+++ src/eval.py
@@ -1,5 +1,7 @@
+import json
 import os
 from pathlib import Path
+from threading import local
 from typing import Any
 
 import dagshub
@@ -7,7 +9,11 @@ import hydra
 import pyrootutils  # pyright: ignore[reportMissingImports]
 import pytorch_lightning as pl
 import torch
+from joblib import PrintTime
 from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning.loggers.mlflow import MLFlowLogger
+
+from entity.s3_classifier import S3Classifier
 
 root = pyrootutils.setup_root(
     search_from=__file__,
@@ -16,58 +22,111 @@ root = pyrootutils.setup_root(
     dotenv=True,
 )
 from src import utils  # noqa: E402
-from src.utils.instantiators import instantiate_callbacks  # noqa: E402
-from src.utils.utils import extras, get_metric_value  # noqa: E402
+from src.entity.aws_secrets import AwsSecrets  # noqa: E402
+from src.entity.best_cnn_model import BestCNNModel, ModelConfig  # noqa: E402
+from src.utils.utils import extras  # noqa: E402
 
 torch.set_float32_matmul_precision("medium")
 log = utils.get_pylogger(__name__)
 
 
-def evaluate(cfg: DictConfig) -> dict[str, Any]:
-    assert cfg.ckpt_path, "Checkpoint path `cfg.ckpt_path` must be provided in the configuration."
+# TODO: 1. fetch model from aws s3
+# 2. compare the performance with local model
+# 3. if local model has better performance, upload to aws
 
-    # Resolve the checkpoint path to an absolute path.
-    # Assumes cfg.ckpt_path is relative to cfg.paths.root_dir if not already absolute.
-    resolved_ckpt_path = Path(cfg.ckpt_path).resolve() if Path(cfg.ckpt_path).is_absolute() else (Path(cfg.paths.root_dir) / cfg.ckpt_path).resolve()
 
-    if not resolved_ckpt_path.is_file():
-        raise FileNotFoundError(f"Checkpoint file not found or is not a file: {resolved_ckpt_path}")
+def evaluate_model(logger: MLFlowLogger, cfg: DictConfig, check_point_path: str) -> dict[str, Any]:
+    """
+    Evaluate the model with the given configuration.
 
-    dagshub.init(repo_owner=os.environ.get("DAGSHUB_REPO_OWNER"), repo_name=os.environ.get("DAGSHUB_REPO_NAME"), mlflow=True)
+    Args:
+    - cfg (DictConfig): The Hydra configuration dictionary.
 
-    log.info("Instantiating loggers...")
-    ml_logger = hydra.utils.instantiate(cfg.logger, tracking_uri=os.environ.get("DAGSHUB_TRACKING_URI"))
-
-    input_shape = [3] + hydra.utils.instantiate(cfg.data.train_preprocess_transforms[1].size)
-    OmegaConf.update(cfg.model.net, "input_shape", input_shape, merge=True)
-
+    Returns:
+    - dict[str, Any]: The evaluation metrics.
+    """
     log.info(f"Instantiating model <{cfg.model._target_}>")
     model: pl.LightningModule = hydra.utils.instantiate(cfg.model)
     model.compile_model()  # (must match the state before loading the checkpoint)
-
     model.eval()
 
     log.info(f"Instantiating data_module <{cfg.datamodule._target_}>")
     data_module: pl.LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
 
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: pl.Trainer = hydra.utils.instantiate(cfg.trainer, logger=ml_logger)
-    object_dict = {
-        "cfg": cfg,
-        "datamodule": data_module,
-        "model": model,
-        "logger": ml_logger,
-        "trainer": trainer,
-    }
-
-    if ml_logger:
-        log.info("Logging hyperparameters!")
-        ml_logger.log_hyperparams(object_dict)
+    trainer: pl.Trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)
 
     log.info("Starting testing with pre-loaded model state.")
-    trainer.test(model=model, datamodule=data_module, ckpt_path=resolved_ckpt_path)  # ckpt_path is None as we've loaded it manually
+    trainer.test(model=model, datamodule=data_module, ckpt_path=check_point_path)  # ckpt_path is None as we've loaded it manually
 
     return trainer.callback_metrics
+
+
+def evaluate(cfg: DictConfig) -> tuple[dict[str, Any], dict[str, Any]]:
+    """
+    Evaluate the model with the given configuration.
+
+    Args:
+    - cfg (DictConfig): The Hydra configuration dictionary.
+
+    Returns:
+    - dict[str, Any]: The evaluation metrics.
+
+    Raises:
+    - FileNotFoundError: If the checkpoint file is not found or is not a file.
+    """
+    dagshub.init(repo_owner=os.environ.get("DAGSHUB_REPO_OWNER"), repo_name=os.environ.get("DAGSHUB_REPO_NAME"), mlflow=True)
+
+    log.info("Instantiating loggers...")
+    ml_logger = hydra.utils.instantiate(cfg.logger, tracking_uri=os.environ.get("DAGSHUB_TRACKING_URI"))
+    input_shape = [3] + hydra.utils.instantiate(cfg.data.train_preprocess_transforms[0].size)
+    cfg.model.net.input_shape = input_shape
+
+    # --- get local model ---
+    best_model_json_path = Path(cfg.paths.best_model_path)
+    best_model_json_file = best_model_json_path / cfg.paths.best_model_json_name
+    assert best_model_json_file.is_file(), "Best model json does not exist"
+
+    with open(str(best_model_json_file)) as f:
+        local_best_model = BestCNNModel.model_validate(json.load(f))
+
+    local_model_metrics = evaluate_model(logger=ml_logger, cfg=cfg, check_point_path=local_best_model.check_point_path)
+
+    # --- get aws model ---
+    secrets = AwsSecrets()
+    s3_classifier = S3Classifier(
+        bucket_name=secrets.bucket_name, cloud_model_key=cfg.paths.cloud_model_key, cloud_model_save_path=cfg.paths.cloud_model_save_path
+    )
+    is_model_present = s3_classifier.is_model_present(model_name=cfg.paths.cloud_model_key)
+    if is_model_present:
+        log.info(f"cloud model {cfg.paths.cloud_model_key} is available")
+
+        s3_classifier.fetch_model_weights()
+
+        dwnloaded_ckpt_path = Path(cfg.paths.cloud_model_save_path)
+        assert dwnloaded_ckpt_path.is_file(), f"checkpoint file is not downloaded at {dwnloaded_ckpt_path}"
+        cloud_model_metrics = evaluate_model(logger=ml_logger, cfg=cfg, check_point_path=cfg.paths.cloud_model_save_path)
+
+        log.info(f"local model metrics: {local_model_metrics}")
+        local_f1_score = local_model_metrics["test/f1_score"].item()
+        cloud_f1_score = cloud_model_metrics["test/f1_score"].item()
+        f1_score_diff = local_f1_score - cloud_f1_score
+        log.info(f"local model f1_score: {local_f1_score}")
+        log.info(f"cloud model f1_score: {cloud_f1_score}")
+        log.info(f"f1_score_diff: {f1_score_diff}")
+
+    else:
+        log.info("cloud model is not available")
+        f1_score_diff = 0
+        cloud_model_metrics = {}
+
+    if (f1_score_diff > cfg.model_perf_diff_threshold) or not is_model_present:
+        # local model is better push to aws
+        log.info("local model is better than cloud model")
+        s3_classifier.upload_model(from_file=local_best_model.check_point_path)
+        log.info("local model pushed to aws")
+
+    return local_model_metrics, cloud_model_metrics
 
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="eval.yaml")
